886367fcbaba268ca419523e0d0cde1e7a3c0208
Adam Martin-Côté <adammartincote@gmail.com>
2019-09-05 14:39:01 -0400

    add test cases from enoncé

--- src/test/test_rushhour.py
+++ src/test/test_rushhour.py
@@ -1,5 +1,21 @@
 from unittest import TestCase
+from src.rushhour import Rushhour
+from src.state import State
 
 
 class TestRushhour(TestCase):
-    pass
+    def test2(self):
+        rh = Rushhour([True, True, False, False, True, True, False, False],
+                      [2, 2, 3, 2, 3, 2, 3, 3],
+                      [2, 0, 0, 0, 5, 4, 5, 3])
+        s = State([1, 0, 1, 4, 2, 4, 0, 1])
+        rh.init_positions(s)
+        b = True
+        print(rh.free_pos)
+        ans = [[False, False, True, True, True, False], [False, True, True, False, True, False],
+               [False, False, False, False, True, False],
+               [False, True, True, False, True, True], [False, True, True, True, False, False],
+               [False, True, False, False, False, True]]
+        b = b and (rh.free_pos[i, j] == ans[i, j] for i in range(6) for j in range(6))
+        print("\n", "résultat correct" if b else "mauvais résultat")
+
