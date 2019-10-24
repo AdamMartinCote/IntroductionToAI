918bd56a7a7eaf3f0fb9399e622330b28045c379
Adam <amcote@gosecure.net>
2019-10-24 08:48:40 -0400

    add initial tp2 files

---
+++ tp2/tests/state.py
@@ -0,0 +1,35 @@
+from unittest import TestCase
+
+from tp2.src.rushhour import Rushhour
+from tp2.src.state import State
+
+
+class TestState(TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def testRocks(self):
+        rh = Rushhour([True, False, True, False, False, True, False, True, False, True, False, True],
+                      [2, 2, 3, 2, 3, 2, 2, 2, 2, 2, 2, 3],
+                      [2, 2, 0, 0, 3, 1, 1, 3, 0, 4, 5, 5],
+                      ["rouge", "vert clair", "jaune", "orange", "violet clair", "bleu ciel", "rose", "violet", "vert",
+                       "noir", "beige", "bleu"])
+        s0 = State([1, 0, 3, 1, 1, 4, 3, 4, 4, 2, 4, 1])
+
+        s1 = s0.put_rock((4, 4))
+        s2 = s1.put_rock((3, 2))
+
+        print("État initial")
+        rh.print_pretty_grid(s0)
+        print(rh.free_pos)
+        print('\n')
+
+        print("Roche 4-4")
+        rh.print_pretty_grid(s1)
+        print(rh.free_pos)
+        print('\n')
+
+        print("Roche 3-2")
+        rh.print_pretty_grid(s2)
+        print(rh.free_pos)
+        print('\n')
