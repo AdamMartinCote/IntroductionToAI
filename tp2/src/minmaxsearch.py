a19f33a499555594ba4c3eabf89dd62c9278ad43
Yoan Gauthier <yoangau@gmail.com>
2019-11-04 19:12:51 -0500

    try to implement minimax_1

--- tp2/src/minmaxsearch.py
+++ tp2/src/minmaxsearch.py
@@ -1,13 +1,33 @@
+import sys
+
+from tp2.src.state import State
+
+
 class MiniMaxSearch:
     def __init__(self, rushHour, initial_state, search_depth):
         self.rushhour = rushHour
         self.state = initial_state
         self.search_depth = search_depth
 
-    def minimax_1(self, current_depth, current_state):
-        # TODO
+    def minimax_1(self, current_depth: int, current_state: State):
+        # todo: je suis vraiment pas certain de ca
+
+        self.rushhour.state = current_state
+
+        if current_depth is self.search_depth:
+            return current_state, current_state.score_heuristic_1(self.rushhour.free_pos)
+
+        possible_states = self.rushhour.possible_moves()
+
         best_move = None
-        return best_move
+        best_score = - (sys.maxsize - 1)
+        for possible_state in possible_states:
+            tmp_move, tmp_score = self.minimax_1(current_depth + 1, possible_state)
+            if tmp_score > best_score:
+                best_score = tmp_score
+                best_move = tmp_move
+
+        return best_move, best_score
 
     def minimax_2(self, current_depth, current_state, is_max):
         # TODO
@@ -25,20 +45,19 @@ class MiniMaxSearch:
         return best_move
 
     def decide_best_move_1(self):
-        pass # TODO
+        pass  # TODO
 
     def decide_best_move_2(self, is_max):
-        pass # TODO
+        pass  # TODO
 
     def decide_best_move_pruning(self, is_max):
-        pass # TODO
+        pass  # TODO
 
     def decide_best_move_expectimax(self, is_max):
-        pass # TODO
+        pass  # TODO
 
     def solve(self, state, is_singleplayer):
-        pass # TODO
+        pass  # TODO
 
     def print_move(self, is_max, state):
-        pass # TODO
-
+        pass  # TODO
