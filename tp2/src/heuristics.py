729d42ecce6b6c8e3cea5400c6a19b2fc14ce9d7
Adam Martin-Côté <adammartincote@gmail.com>
2019-11-10 19:27:01 -0500

    blocking heuristic

--- tp2/src/heuristics.py
+++ tp2/src/heuristics.py
@@ -10,7 +10,13 @@ class Heuristics:
     def blocking(self, state) -> int:
         grid: np.ndarray = self.rush_hour.get_formatted_grid_and_update_free_pos(state)
         red_car_line = grid[self.rush_hour.move_on[0]]
-        path = red_car_line[state.pos[state.pos[0] + self.rush_hour.length[0]]:]
+        path = []
+        for idx in range(len(red_car_line) - 1, -1, -1):
+            if red_car_line[idx] == b'r':
+                break
+            else:
+                path.append(red_car_line[idx])
+
         nb_blocking = sum([1 if square is not b'-' else 0 for square in path])
         print(red_car_line)
         print(path)
