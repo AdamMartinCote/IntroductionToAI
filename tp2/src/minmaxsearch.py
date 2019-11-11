22743213a3c8f3f3a148c2db2ce59657323e53ce
Yoan Gauthier <yoangau@gmail.com>
2019-11-10 20:59:44 -0500

    grmbl

--- tp2/src/minmaxsearch.py
+++ tp2/src/minmaxsearch.py
@@ -164,6 +164,36 @@ class MiniMaxSearch:
 
         return best_state if current_depth is 0 else current_state
 
+    def max_exp_value(self, current_depth, current_state):
+        # if hash(current_state) in self.visited:
+        #     if self.visited[hash(current_state)] % 2:
+        #         return None
+
+        self.rushhour.state = current_state
+
+        possible_states = self.rushhour.get_possible_moves()
+
+        current_state.score_heuristic_1(self.visited, self.rushhour.free_pos, self.rushhour.length,
+                                        self.rushhour.move_on, self.rushhour.horiz)
+
+        current_score = current_state.score
+
+        if current_depth is self.search_depth or current_state.success():
+            return current_state
+
+        current_state.score = - (sys.maxsize - 1)
+        best_state = None
+        for possible_state in possible_states:
+            possible_state.previous_state = None
+            possible_state.nb_moves -= 1  # because it adds one in get_possible_moves
+            tmp_state = self.exp_value(current_depth + 1, possible_state)
+            tmp_state.score += current_score
+            if current_state.score < tmp_state.score:
+                current_state.score = tmp_state.score
+                best_state = tmp_state
+
+        return best_state if current_depth is 0 else current_state
+
     def exp_value(self, current_depth, current_state):
 
         self.rushhour.state = current_state
@@ -180,14 +210,91 @@ class MiniMaxSearch:
 
         current_state.score = 0
 
+        tmp_states = []
+
         for possible_state in possible_states:
             possible_state.previous_state = None
             possible_state.nb_moves -= 1  # because it adds one in get_possible_moves
             tmp_state = self.max_value(current_depth + 1, possible_state)
             current_state.score += current_score + tmp_state.score * p
+            tmp_states.append(tmp_state)
+
         import random
-        random_index = random.randrange(len(possible_states))
-        random_state = possible_states[random_index]
+        random_index = random.randrange(len(tmp_states))
+        random_state = tmp_states[random_index]
+        random_state.score = current_state.score
+
+        return random_state if current_depth is 0 else current_state
+
+    def exp_value_optimistic(self, current_depth, current_state):
+
+        self.rushhour.state = current_state
+
+        possible_states = self.rushhour.possible_rock_moves()
+        current_state.score_heuristic_1(self.visited, self.rushhour.free_pos, self.rushhour.length,
+                                        self.rushhour.move_on, self.rushhour.horiz)
+
+        current_score = current_state.score
+
+        if current_depth is self.search_depth or current_state.success():
+            return current_state
+
+        current_state.score = 0
+
+        tmp_states = []
+
+        for possible_state in possible_states:
+            possible_state.previous_state = None
+            possible_state.nb_moves -= 1  # because it adds one in get_possible_moves
+            tmp_state = self.max_value(current_depth + 1, possible_state)
+            tmp_states.append(tmp_state)
+
+        scores = [t_s.score + current_score for t_s in tmp_states]
+        ps = [1-(score / sum(scores)) for score in scores]
+
+        for p, score in zip(ps, scores):
+            current_state.score += score * p
+
+        import random
+        random_index = random.randrange(len(tmp_states))
+        random_state = tmp_states[random_index]
+        random_state.score = current_state.score
+
+        return random_state if current_depth is 0 else current_state
+
+    def exp_value_pessimistic(self, current_depth, current_state):
+
+        self.rushhour.state = current_state
+
+        possible_states = self.rushhour.possible_rock_moves()
+        current_state.score_heuristic_1(self.visited, self.rushhour.free_pos, self.rushhour.length,
+                                        self.rushhour.move_on, self.rushhour.horiz)
+
+        current_score = current_state.score
+
+        if current_depth is self.search_depth or current_state.success():
+            return current_state
+
+        current_state.score = 0
+
+        tmp_states = []
+
+        for possible_state in possible_states:
+            possible_state.previous_state = None
+            possible_state.nb_moves -= 1  # because it adds one in get_possible_moves
+            tmp_state = self.max_value(current_depth + 1, possible_state)
+            tmp_states.append(tmp_state)
+
+        scores = [t_s.score + current_score for t_s in tmp_states]
+        ps = [score / sum(scores) for score in scores]
+
+        for p, score in zip(ps, scores):
+            current_state.score += score * p
+
+        import random
+        random_index = random.randrange(len(tmp_states))
+        random_state = tmp_states[random_index]
+        random_state.score = current_state.score
 
         return random_state if current_depth is 0 else current_state
 
@@ -195,9 +302,9 @@ class MiniMaxSearch:
         return self.max_pruning(current_depth, current_state, alpha, beta) if is_max \
             else self.min_pruning(current_depth, current_state, alpha, beta)
 
-    def expectimax(self, current_depth, current_state, is_max):
-        return self.max_value(current_depth, current_state) if is_max \
-            else self.exp_value(current_depth, current_state)
+    def expectimax(self, current_depth, current_state, is_max, exp_func):
+        return self.max_exp_value(current_depth, current_state) if is_max \
+            else exp_func(current_depth, current_state)
 
     def decide_best_move_1(self):
         """
@@ -247,9 +354,17 @@ class MiniMaxSearch:
             else:
                 self.visited[hash(init_state)] += 1
 
-    def decide_best_move_expectimax(self, is_max):
+    def decide_best_move_expectimax(self, is_max, is_pessimistic=False, is_optimistic=False):
         init_state = self.rushhour.state
-        best_move = self.expectimax(0, self.rushhour.state, is_max)
+        best_move = None
+
+        if is_pessimistic:
+            best_move = self.expectimax(0, self.rushhour.state, is_max, self.exp_value_pessimistic)
+        elif is_optimistic:
+            best_move = self.expectimax(0, self.rushhour.state, is_max, self.exp_value_optimistic)
+        else:
+            best_move = self.expectimax(0, self.rushhour.state, is_max, self.exp_value)
+
         self.rushhour.state = init_state
         self.rushhour.update_free_pos()
 
@@ -295,10 +410,10 @@ class MiniMaxSearch:
                 self.rushhour.plot_free_pos()
                 print(s)
 
-    def solve_expectimax(self, verbose=True):
+    def solve_expectimax(self, verbose=True, is_pessimistic=False, is_optimistic=False):
         is_max = True
         while not self.rushhour.state.success():
-            self.decide_best_move_expectimax(is_max)
+            self.decide_best_move_expectimax(is_max, is_pessimistic, is_optimistic)
             s = self.str_move(is_max, self.rushhour.state)
 
             is_max = not is_max
--- tp2/tests/testexpectimax.py
+++ tp2/tests/testexpectimax.py
@@ -14,13 +14,13 @@ class TestExpectimax(unittest.TestCase):
     def setUpClass(self) -> None:
         self.table_maker = TableMaker(file_name='expectimax.csv', header=['test_name', 'time (ms)'])
 
-    def execute_expectimax(self, test_name):
+    def execute_expectimax(self, test_name, is_pessimistic=False, is_optimistic=False):
         rush_hour: RushHour = RushHour(*self.rush_hour_data)
         rush_hour.state = State(self.state_data)
         algo = MiniMaxSearch(rush_hour, rush_hour.state, 3)
         algo.rushhour.update_free_pos()
         start = time()
-        algo.solve_expectimax(verbose=False)
+        algo.solve_expectimax(verbose=False, is_pessimistic=is_pessimistic, is_optimistic=is_optimistic)
         duration = (time() - start) * 1000
         self.table_maker.append_row([test_name, f'{duration:.2f}'])
         print(rush_hour.state.nb_moves)
@@ -40,6 +40,36 @@ class TestExpectimax(unittest.TestCase):
         self.rush_hour_data = rush_hour_data_3
         self.state_data = state_data_3
         self.execute_expectimax(unittest.TestCase.id(self).split('.')[-1])
+
+    def test_expectimax_pessimistic_1(self):
+        self.rush_hour_data = rush_hour_data_1
+        self.state_data = state_data_1
+        self.execute_expectimax(unittest.TestCase.id(self).split('.')[-1], is_pessimistic=True)
+
+    def test_expectimax_pessimistic_2(self):
+        self.rush_hour_data = rush_hour_data_2
+        self.state_data = state_data_2
+        self.execute_expectimax(unittest.TestCase.id(self).split('.')[-1], is_pessimistic=True)
+
+    def test_expectimax_pessimistic_3(self):
+        self.rush_hour_data = rush_hour_data_3
+        self.state_data = state_data_3
+        self.execute_expectimax(unittest.TestCase.id(self).split('.')[-1], is_pessimistic=True)
+
+    def test_expectimax_optimistic_1(self):
+        self.rush_hour_data = rush_hour_data_1
+        self.state_data = state_data_1
+        self.execute_expectimax(unittest.TestCase.id(self).split('.')[-1], is_optimistic=True)
+
+    def test_expectimax_optimistic_2(self):
+        self.rush_hour_data = rush_hour_data_2
+        self.state_data = state_data_2
+        self.execute_expectimax(unittest.TestCase.id(self).split('.')[-1], is_optimistic=True)
+
+    def test_expectimax_optimistic_3(self):
+        self.rush_hour_data = rush_hour_data_3
+        self.state_data = state_data_3
+        self.execute_expectimax(unittest.TestCase.id(self).split('.')[-1], is_optimistic=True)
 
 
 if __name__ == '__main__':
