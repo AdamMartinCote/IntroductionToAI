8191de273c9b7c8c307ebba39a7a18f8fb2202c7
Adam Martin-Côté <adammartincote@gmail.com>
2019-12-10 14:18:48 -0500

    test one_hot fully

--- tp3/test/testsoftmaxclassifier.py
+++ tp3/test/testsoftmaxclassifier.py
@@ -11,7 +11,7 @@ class TestSoftmaxClassifier(TestCase):
         self.softmax = SoftmaxClassifier()
         self.softmax.nb_classes = 6  # les classes possibles sont donc 0-5
 
-    def testOneHot_diagonal_6_6(self):
+    def test_one_hot_1(self):
         y1 = np.array([0, 1, 2, 3, 4, 5])
         y1.shape = (6, 1)
         one_hot = self.softmax._one_hot(y1)
@@ -28,7 +28,7 @@ class TestSoftmaxClassifier(TestCase):
                                         [0, 0, 0, 0, 0, 1]]
                                        ))
 
-    def testOneHot_diagonal_4_1(self):
+    def test_one_hot_2(self):
         y2 = np.array([5, 5, 5, 5])
         y2.shape = (4, 1)
         one_hot = self.softmax._one_hot(y2)
@@ -42,14 +42,16 @@ class TestSoftmaxClassifier(TestCase):
                                         [0, 0, 0, 0, 0, 1]]
                                        ))
 
-        # y3 = np.array([0, 0, 0, 0])
-        # y3.shape = (4, 1)
-        # print('\nTroisième test')
-        # print(softmax._one_hot(y3))
+    def test_one_hot_3(self):
+        y3 = np.array([0, 0, 0, 0])
+        y3.shape = (4, 1)
+        one_hot = self.softmax._one_hot(y3)
 
-        #
-        # Troisième test
-        # [[1. 0. 0. 0. 0. 0.]
-        #  [1. 0. 0. 0. 0. 0.]
-        #  [1. 0. 0. 0. 0. 0.]
-        #  [1. 0. 0. 0. 0. 0.]]
+        print('\nTroisième test')
+        print(one_hot)
+        self.assertTrue(np.array_equal(one_hot,
+                                       [[1, 0, 0, 0, 0, 0],
+                                        [1, 0, 0, 0, 0, 0],
+                                        [1, 0, 0, 0, 0, 0],
+                                        [1, 0, 0, 0, 0, 0]]
+                                       ))
